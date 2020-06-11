2c2cb7bc647c374a50de000bea651c4bdb79c496
Tony Branch <tonybranch.org@yahoo.com>
2020-06-11 23:09:35 +0300

    Template simple WebServer.

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const chalk = require('chalk')
-const text = require('./data')
-console.log(chalk.redBright(text))
-console.log(__dirname)
-console.log(__filename)
+// const chalk = require('chalk')
+// const text = require('./data')
+// console.log(chalk.redBright(text))
+// console.log(__dirname)
+// console.log(__filename)
+
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  })
+  console.log(req.url)
+
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+      if (err) {
+        throw err
+      }
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      res.end(data)
+    })
+  }
+
+  if (req.url === '/contact') {
+    fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+      if (err) {
+        throw err
+      }
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      res.end(data)
+    })
+  }
+})
+
+server.listen(3000, () => {
+  console.log('Server has been started...')
+})
